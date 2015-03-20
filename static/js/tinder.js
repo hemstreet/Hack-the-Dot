5a97f551e3d85669367dacf75a54df2dc974b476
Jiran Dowlati <jiran@Jirans-MacBook-Air-2.local>
2015-03-19 19:39:22 -0600

    Added more stuff.

--- static/js/tinder.js
+++ static/js/tinder.js
@@ -46,21 +46,23 @@ $(document).ready(function(){
     $('#container').append('<div class="buddy" style="display: block;">' +
       '<div class="avatar"  style="display: block; background-image: url(/static/img/' + croissant.img + ')"></div>' +
       '<p class="description">'+ croissant.desc +'</p>' +
+      '<img class="no action" src="/static/img/no.png"/>' +
+      '<img class="yes action" src="/static/img/yes.png"/>' +
     '</div>');
   });
 
-  $(".buddy").on("swiperight",function(){
-    $(this).addClass('rotate-left').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
+    $(".buddy").on("swiperight",function(){
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
 
-    $(this).append('<div class="status like">Like!</div>');
-    if ( $(this).is(':last-child') ) {
-      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-     }
-  });
-
+      $(this).append('<div class="status like">Like!</div>');
+      if ( $(this).is(':last-child') ) {
+        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });
+  
    $(".buddy").on("swipeleft",function(){
     $(this).addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
