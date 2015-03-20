5b07b0b63cbb6e8faacafc64cc05ed4ba1212f88
Jiran Dowlati <jiran@Jirans-MacBook-Air-2.local>
2015-03-19 19:02:56 -0600

    Added frontend code shindigs

--- server.go
+++ server.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 	log.Println("Server started: http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
---
+++ static/js/tinder.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $(".buddy").on("swiperight",function(){
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+
+      $(this).append('<div class="status like">Like!</div>');      
+      if ( $(this).is(':last-child') ) {
+        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });  
+
+   $(".buddy").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+    $(this).append('<div class="status dislike">Dislike!</div>');
+
+    if ( $(this).is(':last-child') ) {
+     $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+      alert('Na-na!');
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    } 
+  });
+
+});
